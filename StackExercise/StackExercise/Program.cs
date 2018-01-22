9a91f0d2bdbf846b7c47c0e237b9012135e9a950
annadema <annamaria.demartino@gmail.com>
2018-01-22 08:07:34 +0100

    Add files via upload

---
+++ StackExercise/StackExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExercise
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var stack=new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            //stack.Push(null);
+            //Console.WriteLine(stack.Pop());
+
+            stack.Push(4);
+            stack.Clear();
+
+        }
+    }
+}
---
+++ StackExercise/StackExercise/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        public List<object> container { get; set; } = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("null can't be added to stack");
+            container.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (container.Count == 0)
+                throw new InvalidOperationException("Stack empty");
+            var temp = container[container.Count - 1];
+            container.RemoveAt(container.Count - 1);
+            return temp;
+        }
+
+        public void Clear()
+        {
+            container.Clear();
+            Console.WriteLine("Stack cleared  {0}", container.Count);
+        }
+    }
+}
